3552d5476a4117cc03f881d55986415ee23443a6
chintanmarvaniya1 <chintanmarvaniya985@gmail.com>
2024-03-17 19:12:08 +0530

    adding cors

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const user = require("./routes/userRoutes");
 const quote = require("./routes/quoteRoutes");
 const dailyCronJobs = require("./utilities/dailyCronJobs");
 const DOBCronJob = require("./utilities/DOBCronJobs");
+var cors = require('cors');
+
 
 dailyCronJobs()
 DOBCronJob()
 
 app.use(express.json());
+app.use(cors());
 app.use('/api',user)
 app.use('/api',quote)
 
